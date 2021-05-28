a5f82f0cd1f27003bde0eed57b52a2c1f530acca
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-05-28 08:03:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
 
 import os
+import logging
 import discord
 from discord.ext import commands
-from discord.ext.commands import CommandNotFound
 from discord_slash import SlashCommand
-from datetime import datetime
 from dotenv import load_dotenv
-import settings
+import warmup
 
-settings.init()
+# Setup Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+# Warmup and global variables
 load_dotenv()
+warmup.init()
+warmup.cachegifs()
 token = os.getenv('key')
-# construct bot
-bot = commands.Bot(command_prefix='*')
-slash = SlashCommand(bot, override_type=True, sync_commands=True)
-
-
-# get boot date and time
-time = datetime.now()
-uptime = time.strftime("%d/%m/%Y %H:%M:%S")
-
+# Start Setting up the bot
+intent = discord.Intents.default()
+intent.members = True
+intent.guilds = True
+bot = commands.Bot(command_prefix='-', help_command=None,
+                   description='Just a Discord Bot', intents=intent)
+slash = SlashCommand(bot, sync_commands=True,
+                     sync_on_cog_reload=True, override_type=True)
 # Load Cogs
 for filename in os.listdir('cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 # bot.load_extension(f'cogs.Others')
-
-
-# Handle common errors
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        await ctx.send(f'{ctx.author.mention} Command not found')
-        print(f'{datetime.now()} {ctx.author} has sent a bad command')
-        return
-    if isinstance(error, commands.MemberNotFound):
-        await ctx.send(f'{ctx.author.mention} User not found')
-        print(f'{datetime.now()} {ctx.author} has sent a bad argument')
-        return
-    raise error
-
-
 @bot.event
 async def on_ready():
     print('Bot {0.user}'.format(bot))
