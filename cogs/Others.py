e25d7652619ce746059dd76d1b687e71fb6070a7
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-06-15 09:58:00 -0600

    added description for duplicate id

--- cogs/Others.py
+++ cogs/Others.py
@@ -27,7 +27,7 @@ class Others(commands.Cog):
         await ctx.send(embed=messages.hug(ctx, member))
 
     @cog_ext.cog_slash(name="funfix",description="General Form to fix any issues related to the FunServer",guild_ids=funserver,options=[
-        create_option(name="ingame",description="your character name inside the fun server",option_type=3,required=True),create_option(name="quest",description="name or id of the quest that you need fixed",option_type=3,required=True)])
+        create_option(name="ingame",description="your character name inside the fun server",option_type=3,required=True),create_option(name="quest",description="name or id of the quest that you need fixed if \"key already added error\" type duplicate item",option_type=3,required=True)])
     async def funfix(self, ctx:SlashContext,ingame:str,quest:str):
         msg=discord.Embed(title=f" FunServer Fix: {ctx.author}",description=f"Ingame Name: {ingame} \n Quest Id or Name: {quest}",timestamp=datetime.now(),color=warmup.rmd())
         log=self.bot.get_channel(823736552341241886) #cfgg-dev-testing
