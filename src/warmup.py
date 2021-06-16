95b983e71629ae8282a9e2df5b325eec9360259d
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-06-15 21:59:55 -0600

    made gif caching asyncronous

--- src/warmup.py
+++ src/warmup.py
@@ -1,7 +1,6 @@
 import os
 import json
 import random
-import requests
 
 
 def init():
@@ -14,43 +13,3 @@ def init():
 def rmd():
     r = random.randint(0, 0xFFFFFF)
     return r
-
-def cachegifs():
-    limits = 50
-    hellolimit = 10
-    # caching 50 hugs
-    r = requests.get(
-        "https://g.tenor.com/v1/search?q=hug&key=%s&limit=%s" % (tenorapi, limits))
-    if r.status_code == 200:
-        huggifs = []
-        data = json.loads(r.content)['results']
-        for item in data:
-            huggifs.append(item['media'][0]['gif']['url'])
-        with open('images/hug.json', 'r+') as file:
-            file.seek(0)
-            json.dump(huggifs, file)
-            file.truncate()
-   # caching 50 bonks
-    r = requests.get(
-        "https://g.tenor.com/v1/search?q=bonk&key=%s&limit=%s" % (tenorapi, limits))
-    if r.status_code == 200:
-        bonkgis = []
-        data = json.loads(r.content)['results']
-        for item in data:
-            bonkgis.append(item['media'][0]['gif']['url'])
-        with open('images/bonk.json', 'r+') as file:
-            file.seek(0)
-            json.dump(bonkgis, file)
-            file.truncate()
-   # caching 10 hi's
-    r = requests.get(
-        "https://g.tenor.com/v1/search?q=hi&key=%s&limit=%s" % (tenorapi, hellolimit))
-    if r.status_code == 200:
-        hellogifs = []
-        data = json.loads(r.content)['results']
-        for item in data:
-            hellogifs.append(item['media'][0]['gif']['url'])
-        with open('images/hello.json', 'r+') as file:
-            file.seek(0)
-            json.dump(hellogifs, file)
-            file.truncate()
