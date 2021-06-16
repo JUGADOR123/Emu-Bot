498e6c6c8c80e2f6b6bdc310e6cad39278d5697c
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-06-16 12:10:34 -0600

    added anti steam scam links

--- cogs/events.py
+++ cogs/events.py
@@ -1,5 +1,5 @@
-import json
-import os
+import re
+from difflib import SequenceMatcher
 import discord
 from datetime import datetime
 from discord_components.client import DiscordComponents
@@ -37,6 +37,24 @@ class events(commands.Cog):
     #@commands.Cog.listener()
     #async def on_button_click(self,res):
     #    await res.respond(type=InteractionType.ChannelMessageWithSource,content=f"{res.component.label} pressed")
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        if message.author == message.author.bot:
+            return
+        regex = re.compile(r'(s(?!:).{10,17}y)')
+        content = message.content
+        match = re.findall(regex, content)
+        for entry in match:
+            likeness = SequenceMatcher(None, 'steamcommunity', entry).ratio()
+            scam_match = re.search(
+                rf'(https?://)?{re.escape(entry)}(\.(ru|com))', content)
+            if scam_match and 0.76 <= likeness < 1:
+                print(
+                    f'{str(datetime.now().time())}| Message Deleted from: {message.author}')
+                await message.delete()
+                await message.author.send(f"You Have been token logged, please change your password to prevent it from happening")
 
 def setup(bot):
     bot.add_cog(events(bot))
