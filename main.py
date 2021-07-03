392ccf8b2b12847797a13a104a6783e503d2be75
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-07-03 11:17:16 -0600

    base of bot
    added loading art
    added creation of directories
    added check if images directories are present
    updated requirements.txt
    updated gitignore

---
+++ main.py
@@ -0,0 +1,36 @@
+import json
+import os
+from time import sleep
+
+art = """\
+███████ ███    ███ ██    ██     ██████   ██████  ████████     
+██      ████  ████ ██    ██     ██   ██ ██    ██    ██        
+█████   ██ ████ ██ ██    ██     ██████  ██    ██    ██        
+██      ██  ██  ██ ██    ██     ██   ██ ██    ██    ██        
+███████ ██      ██  ██████      ██████   ██████     ██        """
+
+
+def startup() -> bool:
+    try:
+        if not os.path.exists("images"):
+            print("Creating Directories...")
+            os.makedirs("images")
+            empty = []
+            data = json.dumps(empty)
+            with open(os.path.join("images", "bonk.json"), "x") as temp:
+                temp.write(data)
+            with open(os.path.join("images", "hug.json"), "x") as temp:
+                temp.write(data)
+            print("Directories Created")
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    print(art)
+    sleep(5)
+    print("Loading Startup")
+    startup()
+    print("Startup Loaded")
