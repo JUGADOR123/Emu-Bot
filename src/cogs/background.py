241fc25f2e1053a4239282684fefc2f58a8855c0
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-07-03 14:45:58 -0600

    added background tasks

---
+++ src/cogs/background.py
@@ -0,0 +1,41 @@
+import json
+import src.warmup as warmup
+from discord.ext import tasks,commands
+import aiohttp
+
+limits=50
+
+
+class background(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.cachegifs.start()
+
+    @tasks.loop(hours=1)
+    async def cachegifs(self):
+        print("Caching Gifs")
+        huggifs = []
+        bonkgifs = []
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://g.tenor.com/v1/search?q=hug&key=%s&limit=%s" % (warmup.tenorapi, limits)) as response:
+                data = json.loads(await response.text())['results']
+                for item in data:
+                    huggifs.append(item['media'][0]['gif']['url'])
+                    with open('images/hug.json', 'r+') as file:
+                        file.seek(0)
+                        json.dump(huggifs, file)
+                        file.truncate()
+            async with session.get("https://g.tenor.com/v1/search?q=bonk&key=%s&limit=%s" % (warmup.tenorapi, limits)) as response:
+                data = json.loads(await response.text())['results']
+                for item in data:
+                    bonkgifs.append(item['media'][0]['gif']['url'])
+                    with open('images/bonk.json', 'r+') as file:
+                        file.seek(0)
+                        json.dump(bonkgifs, file)
+                        file.truncate()
+        await session.close()
+        print("Gifs Cached")
+
+
+def setup(bot):
+    bot.add_cog(background(bot))
