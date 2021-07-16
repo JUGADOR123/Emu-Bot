f164a7e0e7a64992bd2abe1067661251974aac6f
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-07-16 16:21:14 -0600

    upgraded anti steam scam

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -1,7 +1,5 @@
 import re
 from difflib import SequenceMatcher
-from urllib.parse import urlparse
-
 from discord.ext import commands
 
 
@@ -26,35 +24,29 @@ class events(commands.Cog):
         log1 = self.bot.get_channel(858971025174036540)  # Emutarkov logs
         log2 = self.bot.get_channel(826592098702721065)  # emu related
 
-        regex = re.compile(r"((https)|(http)):\/\/st.*((\.ru)|(\.com))")
-
-        content = message.content
-        match = re.findall(regex, content)
-        if match:
-            foundLinks = re.search("(?P<url>https?://[^\s]+)", content).group("url")
-            parsed = urlparse(url=foundLinks)
-            likeness = SequenceMatcher(
-                None, "steamcommunity.com", parsed.hostname
-            ).ratio()
-            matches = re.findall(regex, foundLinks)
-            for match in matches:
-                if match and likeness < 1 and likeness > 0.65:
-                    result = "{:.3f}".format(likeness)
+        finder = r"(https?://)?(?:www.)?(?P<url>[^\s]{5,20})(?P<domain>(?:.com|.ru))(?P<extra>[^\s]+)?"  # Regex to find any form of links
+        linksFound = re.findall(finder, message.content)
+        if linksFound:
+            for link in linksFound:
+                host = link[1] + link[2]
+                likeness = SequenceMatcher(None, "steamcommunity.com", host).ratio()
+                result = "{:.3f}".format(likeness)
+                if likeness < 1 and likeness > 0.65:
                     print(
-                        f"Message sent by: {message.author} \n Possible Scam link: {parsed.hostname} with a match of: {result}"
+                        f"Message sent by: {message.author} \n Possible Scam link: {host} with a match of: {result}"
                     )
                     await message.delete()
                     if message.guild.id == 826592098702721065:  # post in emu related
                         await log2.send(
-                            f"Message sent by: {message.author.mention} \n Possible Scam link: {parsed.hostname} with a match of: {result}"
+                            f"Message sent by: {message.author.mention} \n Possible Scam link: {host} with a match of: {result}"
                         )
                     else:  # post in emutarkov
                         await log1.send(
-                            f"Message sent by: {message.author.mention} \n Possible Scam link: {parsed.hostname} with a match of: {result}"
+                            f"Message sent by: {message.author.mention} \n Possible Scam link: {host} with a match of: {result}"
                         )
-                    await message.author.send(
-                        f"you were token logged due to a malicious file you have opened, your account is currently being used as a phishing bot in servers. please change your password as your account security is critical"
-                    )
+                        await message.author.send(
+                            f"you were token logged due to a malicious file you have opened, your account is currently being used as a phishing bot in servers. please change your password as your account security is critical"
+                        )
 
 
 def setup(bot):
